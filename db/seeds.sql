c6b60cef2e21532ffbcb357ab655f8dc5b3f9e66
hlkirkness <hlkirkness@gmail.com>
2021-10-21 19:52:08 -0400

    seeds file created

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (id, name)
+VALUES 
+(1, "Sales"),
+(2, "Engineering"),
+(3, "Finance"),
+(4, "Legal");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+  (1, "Lead Sales", 80000, 1),
+  (2, "Junior Sales", 70000, 1),
+  (3, "Lead Engineer", 90000, 2),
+  (4, "Junior Engineer", 85000, 2);
+
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+  (1, "Peter", "Larson", 1, NULL),
+  (2, "Harrison", "Trider", 2, 1),
+  (3, "Austin", "Schwarm", 2, 1),
+  (4, "Matthew", "DePietro", 3, NULL),
+  (5, "Thomas", "Chadwick", 4, 4);
