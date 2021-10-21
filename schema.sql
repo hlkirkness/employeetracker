952599052a290d20d86eb06b3359652c80762e97
hlkirkness <hlkirkness@gmail.com>
2021-10-21 18:51:16 -0400

    tables finished

--- schema.sql
+++ schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    manager_id INT,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
