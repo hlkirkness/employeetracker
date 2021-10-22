e6f2d230bfd56c4a312a9295f1b3e960ac2a2660
hlkirkness <hlkirkness@gmail.com>
2021-10-22 12:57:41 -0400

    fixed errors

--- db/connection.js
+++ db/connection.js
@@ -1,25 +1,12 @@
-const mysql = require('mysql2');
-const inquirer = require("inquirer");
+const mysql = require("mysql2");
 const db = mysql.createConnection(
   {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // {TODO: Add your MySQL password}
-    password: 'password',
-    database: 'employeetracker_db'
+    host: "localhost",
+    user: "root",
+    password: "Darren1@",
+    database: "employee_tracker",
   },
-  console.log(`Connected to the employeetracker_db database.`)
+  console.log("Connected to the Employee Watcher database.")
 );
-// Query database
-// let deletedRow = 2;
-// db.query(`DELETE FROM books WHERE id = ?`, deletedRow, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-// Query database
-db.query('SELECT * FROM employee', function (err, results) {
-  console.log(results);
-});
+
+module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,32 @@
-DROP DATABASE IF EXISTS employeetracker_db;
-CREATE DATABASE employeetracker_db;
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
 
-USE employeetracker_db;
+USE employee_tracker;
+
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    manager_id INT,
-    role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+FOREIGN KEY (role_id) REFERENCES role (id),
+manager_id INTEGER NULL,
+FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
